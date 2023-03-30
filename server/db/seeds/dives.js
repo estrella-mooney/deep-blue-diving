dcb954fa1215d2aecf34192e4007bf2b158f252b
Sacha Beaumier <sacha.beaumier@gmail.com>
2023-03-30 17:45:11 +1300

    seeds and migration completed v2

--- server/db/seeds/dives.js
+++ server/db/seeds/dives.js
@@ -9,7 +9,8 @@ exports.seed = async function (knex) {
     {
       id: 1,
       title: 'Poor Knights Islands',
-      description: '',
+      description:
+        'The best diving spot in New Zealand. Here you can find the scuttled vessel of the Rainbow Warrior and lots of Blue Moki',
       max_depth: 10,
       duration: 120,
       max_group: 3,
@@ -18,7 +19,8 @@ exports.seed = async function (knex) {
     {
       id: 2,
       title: 'Ferry Wharfs in Thorndon',
-      description: '',
+      description:
+        'Dirty rubbish and other detritus bobble in the water with mutant fish',
       max_depth: 6,
       duration: 180,
       max_group: 2,
@@ -27,7 +29,8 @@ exports.seed = async function (knex) {
     {
       id: 3,
       title: 'Fiji Islands',
-      description: '',
+      description:
+        'Sunny Island in the South Pacific where you can find all the tropic fish that you may desire. Beautiful white sands and coconut trees abound',
       max_depth: 7,
       duration: 60,
       max_group: 3,
@@ -36,7 +39,8 @@ exports.seed = async function (knex) {
     {
       id: 4,
       title: 'Auckland Islands',
-      description: '',
+      description:
+        'Lots of Snapper deep in Antarctic Water. A lonely outpost in the middle of no where',
       max_depth: 5,
       duration: 120,
       max_group: 6,
@@ -45,7 +49,8 @@ exports.seed = async function (knex) {
     {
       id: 5,
       title: 'Great Barrier Island',
-      description: '',
+      description:
+        'Beautiful subtropical fish with massive schools of Kingfish and Red Cod everywhere',
       max_depth: 15,
       duration: 480,
       max_group: 5,

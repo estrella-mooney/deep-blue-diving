bd955e67dd4d6c3baba73232d73aa81ee40c4ee0
SamMiddleton <sam@fishstat.com>
2023-03-31 12:41:22 +1300

    start making thunks

--- client/actions/joins.ts
+++ client/actions/joins.ts
@@ -1,17 +1,34 @@
 import { Join } from '../../common/join'
+import { getAllJoins } from '../apis/joinApi'
+import { ThunkAction } from '../store'
+import { setError } from './error'
 
 export type JoinAction =
   | { type: 'SET_JOINS' | 'ADD_JOINS'; payload: Join[] }
   | { type: 'DEL_JOINS'; payload: number[] }
 
-export function setJoins(joins: Join[]): JoinAction {
+function setJoins(joins: Join[]): JoinAction {
   return { type: 'SET_JOINS', payload: joins }
 }
 
-export function addJoins(joins: Join[]): JoinAction {
+function addJoins(joins: Join[]): JoinAction {
   return { type: 'ADD_JOINS', payload: joins }
 }
 
-export function delJoins(ids: number[]): JoinAction {
+function delJoins(ids: number[]): JoinAction {
   return { type: 'DEL_JOINS', payload: ids }
 }
+
+export function getJoins(): ThunkAction {
+  return (dispatch) => {
+    return getAllJoins()
+      .then((joins) => {
+        dispatch(setJoins(joins))
+      })
+      .catch((err) => {
+        dispatch(setError(err.message))
+      })
+  }
+}
+
+// export function

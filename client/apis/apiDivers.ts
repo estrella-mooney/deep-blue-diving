6fb1fbca6ac198c84090a3729caa6f6ba525bd97
Shadae <114277579+shadae-ellmers@users.noreply.github.com>
2023-03-31 10:49:38 +1300

    divers api complete

--- client/apis/apiDivers.ts
+++ client/apis/apiDivers.ts
@@ -5,6 +5,20 @@ export function getAllDivers(): Promise<Diver[]> {
   return request.get('/api/v1/divers').then((res) => res.body)
 }
 
+export function addDiver(diver: DiverInfo): Promise<Diver> {
+  return request
+    .post('/api/v1/divers')
+    .send(diver)
+    .then((res) => res.body)
+}
+
 export function updateDiver(diver: Diver): Promise<Diver> {
-  return request.patch('/api/v1/divers/')
+  return request
+    .patch(`/api/v1/divers/${diver.id}`)
+    .send(diver)
+    .then((res) => res.body)
+}
+
+export function delDiver(id: number): Promise<void> {
+  return request.delete(`/api/v1/divers/${id}`).then(() => {})
 }

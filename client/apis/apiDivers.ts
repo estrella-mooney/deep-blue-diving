e1dedb16e7a7789d72aa8e810cdbd7e5d8c4d2eb
sachabeaumier <13222425+sachabeaumier@users.noreply.github.com>
2023-03-31 10:40:57 +1300

    server update

--- client/apis/apiDivers.ts
+++ client/apis/apiDivers.ts
@@ -4,3 +4,7 @@ import { Diver, DiverInfo } from '../../common/diver'
 export function getAllDivers(): Promise<Diver[]> {
   return request.get('/api/v1/divers').then((res) => res.body)
 }
+
+export function updateDiver(diver: Diver): Promise<Diver> {
+  return request.patch('/api/v1/divers/')
+}

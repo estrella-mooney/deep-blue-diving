ba51099ea2d6740532499eda0d6c204255e19894
Shadae <114277579+shadae-ellmers@users.noreply.github.com>
2023-03-31 14:51:31 +1300

    stuff

--- client/components/Dives.tsx
+++ client/components/Dives.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { useAppDispatch, useAppSelector } from '../hooks/redux'
 import { fetchDives } from '../actions/dives'
 import SingleDive from './SingleDive'
+import { Link } from 'react-router-dom'
 
 export default function AllDives() {
   const dispatch = useAppDispatch()
@@ -13,6 +14,7 @@ export default function AllDives() {
 
   return (
     <section>
+      <Link to="/dives/add">Add Dive</Link>
       <div>
         {allDives.map((dives) => (
           <SingleDive divesData={dives} key={dives.id} />

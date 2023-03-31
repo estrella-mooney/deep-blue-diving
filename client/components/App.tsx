68a67269828e98da3e155dce91e9912755b3c5c0
Shadae <114277579+shadae-ellmers@users.noreply.github.com>
2023-03-31 15:26:59 +1300

    actually save changes to app

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,7 +1,6 @@
 import Divers from './Divers'
 import Dives from './Dives'
 import Home from './Home'
-<<<<<<< HEAD
 import DiveForm from './DiveForm'
 import { Link, Route, Routes } from 'react-router-dom'
 import DiverForm from './DiverForm'
@@ -10,11 +9,6 @@ import { useAppDispatch } from '../hooks/redux'
 import { fetchDives } from '../actions/dives'
 import { getJoins } from '../actions/joins'
 import { fetchDivers } from '../actions/divers'
-=======
-import { Route, Routes } from 'react-router-dom'
-import DiverForm from './DiverForm'
-import DiveForm from './DiveForm'
->>>>>>> 590ec247b55492454ed28da320dbd2b9d4aa7a92
 
 function App() {
   const dispatch = useAppDispatch()
@@ -36,13 +30,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/divers" element={<Divers />} />
           <Route path="/dives" element={<Dives />} />
-<<<<<<< HEAD
           <Route path="/dives/add" element={<DiveForm />} />
           <Route path="/divers/add" element={<DiverForm />} />
-=======
-          <Route path="/divers/add" element={<Divers />} />
-          <Route path="/dives/add" element={<DiveForm />} />
->>>>>>> 590ec247b55492454ed28da320dbd2b9d4aa7a92
           {/* <Route path="/divers/add" element={<UpdateDiver />} />
           <Route path="/divers/add" element={<UpdateDive />} /> */}
         </Routes>

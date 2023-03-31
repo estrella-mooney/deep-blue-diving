e77ed1372eb518485d8fda4b499598b78d608e88
Shadae <114277579+shadae-ellmers@users.noreply.github.com>
2023-03-31 14:34:35 +1300

    router stuff

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,19 +1,24 @@
 import Divers from './Divers'
+import Dives from './Dives'
+import Home from './Home'
 import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <header className="header">
+      <header>
         <h1>Dive One Oh One Application</h1>
       </header>
-      <section className="main">
-        {/* <Routes>
-          <Route path='/' element={<Divers divers={divers}/>}>
-
-          </Route>
-          
-        <Routes/> */}
+      <section>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/divers" element={<Divers />} />
+          <Route path="/dives" element={<Dives />} />
+          <Route path="/divers/add" element={<Divers />} />
+          <Route path="/divers/add" element={<Dives />} />
+          {/* <Route path="/divers/add" element={<UpdateDiver />} />
+          <Route path="/divers/add" element={<UpdateDive />} /> */}
+        </Routes>
       </section>
     </>
   )
